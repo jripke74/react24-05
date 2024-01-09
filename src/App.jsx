05a1f1e3c6bf3b6cfafed1685ec7a3549e40ff29
Jeff Ripke <jripke74@gmail.com>
2024-01-09 11:49:04 -0800

    add classes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from "./components/ProjectSidebar.jsx";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSidebar />
+    </main>
+  );
+}
+
+export default App;
