b5587db029dcf419a9b6ddf572b0b749a07aca24
Jeff Ripke <jripke74@gmail.com>
2024-01-12 08:06:25 -0800

    add style to modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -5,7 +5,7 @@ import Modal from "./Modal.jsx";
 
 const classes = ""
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const title = useRef();
@@ -20,7 +20,7 @@ export default function NewProject({ onAdd }) {
     if (
       enteredTitle.trim() === '' ||
       enteredDescription.trim() === '' ||
-      enteredDueDate() === ''
+      enteredDueDate.trim() === ''
     ) {
       modal.current.open();
       return;
@@ -36,14 +36,18 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Okay">
-        <h2>Invalid Input</h2>
-        <p>Oops ... looks like you forgot to enter a value.</p>
-        <p>Please make sure you provide a valid value for every input filed.</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input filed.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button>
           </li>
           <li>
             <button
