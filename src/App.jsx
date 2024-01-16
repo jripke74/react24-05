5815ee9feb208b42751bc69eb446eca1c1d7732b
Jeff Ripke <jripke74@gmail.com>
2024-01-16 13:06:59 -0800

    add task delete

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,13 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {
-
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
   }
 
   function handleSelectProject(id) {
@@ -115,6 +120,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectState.selectedProjectId}
       />
       {content}
     </main>
